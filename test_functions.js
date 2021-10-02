9b3bc200b4857f0974cec02b6639514be9622b91
Raul <raul.muser@gmail.com>
2021-10-02 10:33:19 +0300

    Update test_functions.js

--- test_functions.js
+++ test_functions.js
@@ -1,4 +1,4 @@
-function test(func,expected, ...args){
+export default function test(func,expected, ...args){
   console.log(func(...args) === expected);
   let msg_str =` ${func.name} with params [${args.join(' and ')}]`
   let result_message = (func(...args) === expected) ? `Test passed: ${msg_str}`:`Test failed: ${msg_str}`;
