5bf7705dc953db681e85d17a61b026a2b8378f3b
Raul <raul.muser@gmail.com>
2021-10-02 10:24:31 +0300

    Create test_functions.js

---
+++ test_functions.js
@@ -0,0 +1,6 @@
+function test(func,expected, ...args){
+  console.log(func(...args) === expected);
+  let msg_str =` ${func.name} with params [${args.join(' and ')}]`
+  let result_message = (func(...args) === expected) ? `Test passed: ${msg_str}`:`Test failed: ${msg_str}`;
+  console.log(result_message);
+}
