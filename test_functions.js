70b60040e0676ffbf7d8c5fabd8272a241850868
Raul <raul.muser@gmail.com>
2021-10-02 10:44:16 +0300

    Update test_functions.js

--- test_functions.js
+++ test_functions.js
@@ -1,6 +1,7 @@
 export default function test(func,expected, ...args){
   console.log(func(...args) === expected);
-  let msg_str =` ${func.name} with params [${args.join(' and ')}]`
-  let result_message = (func(...args) === expected) ? `Test passed: ${msg_str}`:`Test failed: ${msg_str}`;
+  let msg_str =` ${func.name} \n with ${args.map((el, index)=>`arg${index+1}: ${el}`)} \n -`;
+  let result =func(...args);
+  let result_message = (result === expected) ? `Test passed: ${msg_str} ${result} === ${expected}`:`Test failed: ${msg_str} ${result} !== ${expected}`;
   console.log(result_message);
 }
